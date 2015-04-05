d2d0d8d21a804df155f455aefa0236820e3adc1b
rails -v <nudt_houxiang@163.com>
2015-04-05 18:30:58 +0800

    the first commit

---
+++ src/main/java/com/ossean/datamonitor/App.java
@@ -0,0 +1,13 @@
+package com.ossean.datamonitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
